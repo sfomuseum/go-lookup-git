7a386c1c2049534e70a1acbc245f5463c1de6a6a
thisisaaronland <aaron@localhost>
2021-04-05 12:22:03 -0700

    make tests pass

--- git_test.go
+++ git_test.go
@@ -2,20 +2,43 @@ package git
 
 import (
 	"context"
-	"testing"
-	"github.com/sfomuseum/go-lookup"	
+	"encoding/json"
+	"github.com/sfomuseum/go-lookup"
 	"github.com/sfomuseum/go-lookup/catalog"
 	"io"
+	"testing"
 )
 
+type Properties struct {
+	Id   int64  `json:"wof:id"`
+	Name string `json:"wof:name"`
+}
+
+type Feature struct {
+	Properties Properties `json:"properties"`
+}
+
 func AppendFunc(ctx context.Context, c lookup.Catalog, fh io.ReadCloser) error {
+
+	var f *Feature
+
+	dec := json.NewDecoder(fh)
+	err := dec.Decode(&f)
+
+	if err != nil {
+		return err
+	}
+
+	props := f.Properties
+	c.LoadOrStore(props.Name, props.Id)
+
 	return nil
 }
 
 func TestGitLookerUpper(t *testing.T) {
 
 	ctx := context.Background()
-	
+
 	lu, err := NewGitLookerUpper(ctx, "https://github.com/sfomuseum-data/sfomuseum-data-maps.git")
 
 	if err != nil {
@@ -27,10 +50,24 @@ func TestGitLookerUpper(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to create new SyncMapCatalog, %v", err)
 	}
-	
+
 	err = lu.Append(ctx, c, AppendFunc)
 
 	if err != nil {
 		t.Fatalf("Failed to append lookup to catalog, %v", err)
 	}
+
+	key := "SFO (2014)"
+	expected := int64(1477881751)
+
+	v, ok := c.Load(key)
+
+	if !ok {
+		t.Fatalf("Failed to locate key '%s'", key)
+	}
+
+	if v.(int64) != expected {
+		t.Fatalf("Unexpected value. Wanted '%d' but got '%d'", expected, v.(int64))
+	}
+
 }
